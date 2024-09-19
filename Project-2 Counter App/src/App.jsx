97876dc678efa351a525ea43d75cb1966e511bee
sonaljinjala1 <sonaljinjala1@gmail.com>
2024-09-19 18:02:03 +0530

    Counter App With Function Component

---
+++ Project-2 Counter App/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import Counter from "./Counter";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+  function App() {
+    const [no , setNo ] = useState(0);
+
+    const Increment = () => {
+      setNo(no + 1);
+    }
+
+    const Decrement = () => {
+      setNo(no - 1);
+    }
+
+    const Reset = () => {
+      setNo(0);
+    }
+
+    return (
+      <div  align="center">
+        <Counter
+        no = {no}
+        plus = {Increment}
+        minus = {Decrement}
+        reset  = {Reset}
+        />
+      </div>
+    )
+  }
+
+export default App
---
+++ Project-2 Counter App/src/Counter.jsx
@@ -0,0 +1,18 @@
+const Counter = ({ no, plus, minus, reset }) => {
+
+    return (
+        <div className="counter">
+            <h1>Counter App</h1>
+
+            <div className="btn-group  d-flex">
+                <button className=" btn1     btn btn-success" onClick={() => plus()} style={{ paddingBottom: "40px" }}>+</button>
+                <h2> {no}</h2>
+                <button className="  btn1     btn btn-success" onClick={() => minus()}>-</button>
+            </div>
+
+            <button className="btn btn-success" onClick={() => reset()} style={{ fontSize: "25px " , fontWeight: "600" , }}>Reset</button>
+        </div>
+    )
+}
+
+export default Counter;    
